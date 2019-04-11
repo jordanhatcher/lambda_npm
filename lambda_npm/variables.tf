efdc0d77fe8b9817a9072309904992354b46e55d
Jordan Hatcher <jhatch12@gmail.com>
2019-04-10 22:04:38 -0400

    Filled in variable descriptions

--- lambda_npm/variables.tf
+++ lambda_npm/variables.tf
@@ -1,14 +1,25 @@
-variable "function_name" {}
+variable "function_name" {
+  description = "Name of the lambda function"
+}
 
-variable "function_directory" {}
+variable "function_directory" {
+  description = "Relative directory that contains the lambda code as well as a package.json file"
+}
 
-variable "role" {}
+variable "role" {
+  description = "Role ARN for the lambda function"
+}
 
-variable "handler" {}
+variable "handler" {
+  description = "Entrypoint for the lambda function"
+}
 
-variable "runtime" {}
+variable "runtime" {
+  description = "Lambda function runtime"
+}
 
 variable "environment" {
-    type = "map"
-    default = {}
+  description = "Map of environment variable names and values"
+  type = "map"
+  default = {}
 }
